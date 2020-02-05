5374d5ecf4f6088fc33ff259a8cb0144621eb03e
og <ogugu@test.com>
2020-02-05 17:39:49 +0900

    [add] PlayerControllerクラスを追加

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Animator anim;
+    private float speed = 2.0f;
+    private float rotationSpeed = 100.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void LateUpdate()
+    {
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+
+        if (Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp(KeyCode.DownArrow) ||
+            Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            anim.SetBool("walk", false);
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow))
+        {
+            anim.SetBool("walk", true);
+            transform.Translate(0,0,translation);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
+        {
+            anim.SetBool("walk", true);
+            transform.Rotate(0, rotation, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetTrigger("jump");
+        }
+        else if (Input.GetKeyDown(KeyCode.P))
+        {
+            anim.SetTrigger("punch");
+        }
+        else if (Input.GetKeyDown(KeyCode.K))
+        {
+            anim.SetTrigger("kick");
+        }
+
+
+    }
+
+}
