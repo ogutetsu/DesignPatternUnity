f2df11b65883c6a8c1cd07527f70f398609c2bfd
og <ogugu@test.com>
2020-02-05 17:54:56 +0900

    [add] Commandクラスを追加

---
+++ Assets/Scripts/Command.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Command : MonoBehaviour
+{
+    public abstract void Execute(Animator anim);
+
+}
+
+public class PerformJump : Command
+{
+    public override void Execute(Animator anim)
+    {
+        anim.SetTrigger("jump");
+    }
+}
+
+public class DoNothing : Command
+{
+    public override void Execute(Animator anim)
+    {
+        
+    }
+}
+
