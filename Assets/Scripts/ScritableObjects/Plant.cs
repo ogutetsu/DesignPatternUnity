789cf9148093cd408a525b26259fe6b15e871546
og <ogugu@test.com>
2020-02-06 11:49:36 +0900

    [update] Plantに処理を実装
    マウスをクリックしたらInfo表示
    heighのTreeにHitしたらプレイヤーの死亡モーション再生

--- Assets/Scripts/ScritableObjects/Plant.cs
+++ Assets/Scripts/ScritableObjects/Plant.cs
@@ -1,10 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Plant : MonoBehaviour
 {
     [SerializeField] private PlantData info;
 
+    private SetPlantInfo spi;
+
+    void Start()
+    {
+        spi = GameObject.FindWithTag("PlantInfo").GetComponent<SetPlantInfo>();
+    }
+
+    void OnMouseDown()
+    {
+        spi.OpenPlantPanel();
+        spi.planeName.text = info.Name;
+        spi.threatLevel.text = info.Threat.ToString();
+        spi.plantIcon.GetComponent<RawImage>().texture = info.Icon;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player" && info.Threat == PlantData.THREAT.High)
+        {
+            JanePlayerController.dead = true;
+        }
+    }
 
 }
