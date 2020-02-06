067154ef96e68d154c31629aacad579fce8b8a9b
og <ogugu@test.com>
2020-02-06 15:35:32 +0900

    [add] EggEventを追加

---
+++ Assets/Radar/EggEvent.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Event", menuName = "Game Event", order = 52)]
+public class EggEvent : ScriptableObject
+{
+    private  List<EggEventListener> elisteners = new List<EggEventListener>();
+
+    public void Register(EggEventListener listener)
+    {
+        elisteners.Add(listener);
+    }
+
+    public void Unregister(EggEventListener listener)
+    {
+        elisteners.Remove(listener);
+    }
+
+    public void Occurred()
+    {
+        for (int i = 0; i < elisteners.Count; i++)
+        {
+            elisteners[i].OnEventOccurs();
+        }
+    }
+
+}
---
+++ Assets/Radar/EggEventListener.cs
@@ -0,0 +1,25 @@
+﻿using UnityEditor.Compilation;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EggEventListener : MonoBehaviour
+{
+    public EggEvent gEvent;
+    public UnityEvent response;
+
+    private void OnEnable()
+    {
+        gEvent.Register(this);
+    }
+
+    private void OnDisable()
+    {
+        gEvent.Unregister(this);
+    }
+
+    public void OnEventOccurs()
+    {
+        response.Invoke();
+    }
+
+}
