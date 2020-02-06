0256c483fafd57283c4b91d5eca5a3fc3fb7c7f1
og <ogugu@test.com>
2020-02-06 17:25:52 +0900

    [add] ProcCubeを追加

---
+++ Assets/Scripts/Prototype/ProcCubeSpawner.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProcCubeSpawner : MonoBehaviour
+{
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Random.Range(0,100) < 10)
+            ProcCube.CreateCube(this.transform.position);
+        
+    }
+}
