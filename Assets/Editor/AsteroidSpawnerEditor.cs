4eb4452c718b4e819db38991548af0a725e57fbe
og <ogugu@test.com>
2020-02-06 18:05:34 +0900

    [add] AsteroidSpawnerのEditorスクリプトを追加

---
+++ Assets/Editor/AsteroidSpawnerEditor.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+[CustomEditor(typeof(AsteroidSpawner))]
+public class AsteroidSpawnerEditor : Editor
+{
+    private string path;
+    private string assetpath;
+    private string filename;
+
+    void OnEnable()
+    {
+        path = Application.dataPath + "/Asteroid";
+        assetpath = "Assets/Asteroid/";
+        filename = "asteroid_" + System.DateTime.Now.Ticks.ToString();
+    }
+
+    public override void OnInspectorGUI()
+    {
+        AsteroidSpawner asteroidSpawner = (AsteroidSpawner) target;
+        DrawDefaultInspector();
+
+        if (GUILayout.Button("Create Asteroid"))
+        {
+            asteroidSpawner.CreateAsteroid();
+        }
+
+        if (GUILayout.Button("Save Asteroid"))
+        {
+            System.IO.Directory.CreateDirectory(path);
+            Mesh mesh = asteroidSpawner.asteroid.GetComponent<MeshFilter>().sharedMesh;
+            AssetDatabase.CreateAsset(mesh, assetpath + mesh.name + ".asset");
+            AssetDatabase.SaveAssets();
+
+            PrefabUtility.SaveAsPrefabAsset(asteroidSpawner.asteroid, assetpath + filename + ".prefab");
+        }
+
+    }
+
+}
