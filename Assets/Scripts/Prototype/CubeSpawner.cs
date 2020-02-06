2c21b1bdf42945a81ecb0ac091bb71b484a45eb6
og <ogugu@test.com>
2020-02-06 17:18:05 +0900

    [add] CubeSpawnerを追加

---
+++ Assets/Scripts/Prototype/CubeSpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSpawner : MonoBehaviour
+{
+    public GameObject cubeprefab;
+    public GameObject sphereprefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Random.Range(0,100) < 10)
+            Instantiate(cubeprefab, this.transform.position, Quaternion.identity);
+        else if(Random.Range(0,100) < 10)
+            Instantiate(sphereprefab, this.transform.position, Quaternion.identity);
+
+    }
+}
