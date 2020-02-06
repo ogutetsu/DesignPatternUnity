5f77521f66ad4c701a521a2c40e6e5039c648010
og <ogugu@test.com>
2020-02-06 16:55:51 +0900

    [add] MessageControllerを追加

---
+++ Assets/Radar/MessageController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class MessageController : MonoBehaviour
+{
+    private Text message;
+    // Start is called before the first frame update
+    void Start()
+    {
+        message = this.GetComponent<Text>();
+        message.enabled = false;
+    }
+
+    public void SetMessage(GameObject go)
+    {
+        message.text = "You picked up an item!!";
+        message.enabled = true;
+        Invoke("TurnOff", 2);
+    }
+
+    void TurnOff()
+    {
+        message.enabled = false;
+    }
+
+}
