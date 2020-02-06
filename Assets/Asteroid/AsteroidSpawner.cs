56bf5d4645e3547a098c2e25697d40cf47721d2d
og <ogugu@test.com>
2020-02-06 17:54:13 +0900

    [add] AsteroidSpawnerを追加

---
+++ Assets/Asteroid/AsteroidSpawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidSpawner : MonoBehaviour
+{
+
+    public Material material;
+    public GameObject asteroid;
+
+    
+
+
+    public void CreateAsteroid()
+    {
+        asteroid = ProcAsteroid.Clone(this.transform.position);
+        asteroid.GetComponent<MeshRenderer>().sharedMaterial = material;
+    }
+
+}
