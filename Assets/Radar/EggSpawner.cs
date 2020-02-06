c4d416d5db187fda57c10f34db57d0d94459d5e1
og <ogugu@test.com>
2020-02-06 15:35:22 +0900

    [add] EggSpawnerを追加

---
+++ Assets/Radar/EggSpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EggSpawner : MonoBehaviour
+{
+
+    public GameObject eggPrefab;
+    public Terrain terrain;
+    private TerrainData terrainData;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        terrainData = terrain.terrainData;
+        InvokeRepeating("CreateEgg", 1, 0.1f);
+    }
+
+    void CreateEgg()
+    {
+        int x = (int) Random.Range(0, terrainData.size.x);
+        int z = (int) Random.Range(0, terrainData.size.z);
+        Vector3 pos = new Vector3(x, 0, z);
+        pos.y = terrain.SampleHeight(pos) + 10;
+        GameObject egg = Instantiate(eggPrefab, pos, Quaternion.identity);
+        
+    }
+
+}
