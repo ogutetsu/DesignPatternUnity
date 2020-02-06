7c30c4aba0874c55a3a603251d64114fc71d2ea6
og <ogugu@test.com>
2020-02-06 17:44:03 +0900

    [add] ProcSphereを追加

---
+++ Assets/Scripts/Prototype/ProcSphereSpawner.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProcSphereSpawner : MonoBehaviour
+{
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Random.Range(0,100) < 10)
+            ProcSphere.Clone(this.transform.position);
+        
+    }
+}
