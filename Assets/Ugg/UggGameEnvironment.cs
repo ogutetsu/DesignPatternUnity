49477035852b58696fe384da032077f7863a6df9
og <ogugu@test.com>
2020-02-07 10:56:42 +0900

    [add] UggGameEnvironmentクラスを追加

---
+++ Assets/Ugg/UggGameEnvironment.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class UggGameEnvironment
+{
+    private static UggGameEnvironment instance;
+    private List<GameObject> checkpoints = new List<GameObject>();
+    public List<GameObject> Checkpoints
+    {
+        get { return checkpoints; }
+    }
+
+    public static UggGameEnvironment Singleton
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new UggGameEnvironment();
+                instance.Checkpoints.AddRange(GameObject.FindGameObjectsWithTag("Checkpoint"));
+            }
+            return instance;
+        }
+    }
+
+
+}
